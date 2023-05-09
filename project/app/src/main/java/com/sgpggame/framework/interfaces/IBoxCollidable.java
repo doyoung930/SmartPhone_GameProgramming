6e895dd6c512c22214bfcdf3b0054fbca1ef468b
Lee Do Young <doyoung930@gmail.com>
2023-05-09 19:09:23 +0900

    delete

--- project/app/src/main/java/com/sgpggame/framework/interfaces/IBoxCollidable.java
+++
@@ -1,7 +0,0 @@
-package com.sgpggame.framework.interfaces;
-
-import android.graphics.RectF;
-
-public interface IBoxCollidable {
-    public RectF getCollisionRect();
-}
--- project/app/src/main/java/com/sgpggame/framework/interfaces/IRecyclable.java
+++
@@ -1,5 +0,0 @@
-package com.sgpggame.framework.interfaces;
-
-public interface IRecyclable {
-    public void onRecycle();
-}
