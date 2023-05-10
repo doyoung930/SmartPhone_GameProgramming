e8de8b3bd8ca0e1c606e624fe850ee2833605bf7
Lee Do Young <doyoung930@gmail.com>
2023-05-10 22:08:45 +0900

    project

---
+++ saveearth/app/src/main/java/com/sgpggame/save_earth_project/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sgpggame.save_earth_project.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.sgpggame.save_earth_project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
